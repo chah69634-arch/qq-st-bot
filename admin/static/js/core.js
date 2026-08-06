d1a4234f205681cb3c0c6debfe5eae53c03c19fb
Xicika <chah69634@gmail.com>
2026-08-06 21:32:49 +0800

    refactor(admin-ui): label configuration sources

--- admin/static/js/core.js
+++ admin/static/js/core.js
@@ -16,7 +16,7 @@ window.addEventListener('admin-language-changed', () => {
 
 
 const _pageFragmentLoads = new Map();
-const ADMIN_UI_FRAGMENT_VERSION = 'admin-ui-capability-entrances-1';
+const ADMIN_UI_FRAGMENT_VERSION = 'admin-ui-capability-entrances-2';
 
 function getRememberedPage() {
   try {
